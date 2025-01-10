3e4f892ee9bd03ff570c5178caaa9ed46b5f5b33
Nikita Potaliukov <potalyukov@gmail.com>
2025-01-10 18:44:15 +0100

    added alpha animation to compose container

--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainerCompose.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainerCompose.kt
@@ -1,8 +1,15 @@
 package com.example.androidpracticumcustomview.ui.theme
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 
 /*
 Задание:
@@ -15,20 +22,36 @@ import androidx.compose.runtime.LaunchedEffect
 @Composable
 fun CustomContainerCompose(
     firstChild: @Composable (() -> Unit)?,
-    secondChild: @Composable (() -> Unit)?
+    secondChild: @Composable (() -> Unit)?,
+    animationDurationMillis: Int = 2000
 ) {
-    // Блок создания и инициализации переменных
-    // ..
+    val firstAlpha = remember { Animatable(0f) }
+    val secondAlpha = remember { Animatable(0f) }
 
-    // Блок активации анимации при первом запуске
     LaunchedEffect(Unit) {
-        // TODO
-        // ...
+        firstChild?.let { firstAlpha.animateTo(targetValue = 1f, tween(animationDurationMillis)) }
+        secondChild?.let { secondAlpha.animateTo(targetValue = 1f, tween(animationDurationMillis)) }
     }
 
-    // Основной контейнер
-    Box() {
-        // TODO
-        // ...
+    Box(modifier = Modifier.fillMaxSize()) {
+        firstChild?.let {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .alpha(firstAlpha.value)
+            ) {
+                it()
+            }
+        }
+
+        secondChild?.let {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .alpha(secondAlpha.value)
+            ) {
+                it()
+            }
+        }
     }
 }
