e38b4f80363d22c8f7b1cce2cdc9a8237c388ab3
Nikita Potaliukov <potalyukov@gmail.com>
2025-01-10 18:42:27 +0100

    added mock views to compose container

--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/MainScreen.kt
@@ -3,7 +3,9 @@ package com.example.androidpracticumcustomview.ui.theme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,15 +23,14 @@ fun MainScreen() {
                 .fillMaxSize()
                 .padding(paddingValues), contentAlignment = Alignment.Center
         ) {
-
             CustomContainerCompose(
                 firstChild = {
-                    // TODO
-                    // ...
+                    Text("Simple text")
                 },
                 secondChild = {
-                    // TODO
-                    // ...
+                    Button({}) {
+                        Text("I'm a button")
+                    }
                 }
             )
         }
